d16b6ef3d16e5e71adaad30066f480a016bc33c4
Moon Ji-Hee <answlgml6@gmail.com>
2023-04-18 18:59:51 +0900

    2023-04-18

--- app/src/main/java/com/wooriyo/pinmenuer/menu/adpter/GoodsAdapter.kt
+++ app/src/main/java/com/wooriyo/pinmenuer/menu/adpter/GoodsAdapter.kt
@@ -1,17 +1,24 @@
 package com.wooriyo.pinmenuer.menu.adpter
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import com.wooriyo.pinmenuer.MyApplication
 import com.wooriyo.pinmenuer.R
+import com.wooriyo.pinmenuer.config.AppProperties
+import com.wooriyo.pinmenuer.config.AppProperties.Companion.VIEW_TYPE_ADD
+import com.wooriyo.pinmenuer.databinding.ListMenuAddBinding
 import com.wooriyo.pinmenuer.databinding.ListMenuBinding
 import com.wooriyo.pinmenuer.listener.ItemClickListener
 import com.wooriyo.pinmenuer.model.GoodsDTO
 import com.wooriyo.pinmenuer.util.AppHelper
 
-class GoodsAdapter(val dataSet: ArrayList<GoodsDTO>): RecyclerView.Adapter<GoodsAdapter.ViewHolder>() {
+class GoodsAdapter(val dataSet: ArrayList<GoodsDTO>): Adapter<RecyclerView.ViewHolder>() {
     lateinit var itemClickListener: ItemClickListener
 
     var selPos = 0
@@ -20,16 +27,24 @@ class GoodsAdapter(val dataSet: ArrayList<GoodsDTO>): RecyclerView.Adapter<Goods
         this.itemClickListener = itemClickListener
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = ListMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        val bindingAdd = ListMenuAddBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return if (viewType == VIEW_TYPE_ADD) AddViewHolder(bindingAdd) else ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(dataSet[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(getItemViewType(position) == AppProperties.VIEW_TYPE_COM) {
+            holder as ViewHolder
+            (holder as ViewHolder).bind(dataSet[position])
+        } else {
+            holder as AddViewHolder
+            (holder as AddViewHolder).bind()
+        }
 
         if(selPos == position)
-            holder.binding.menu.setBackgroundResource(R.drawable.gradient_main)
+            holder.bin.menu.setBackgroundResource(R.drawable.gradient_main)
         else
             holder.binding.menu.setBackgroundColor(Color.WHITE)
 
@@ -46,7 +61,11 @@ class GoodsAdapter(val dataSet: ArrayList<GoodsDTO>): RecyclerView.Adapter<Goods
     }
 
     override fun getItemCount(): Int {
-        return dataSet.size
+        return dataSet.size +1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(position == itemCount -1) AppProperties.VIEW_TYPE_ADD else AppProperties.VIEW_TYPE_COM
     }
 
     class ViewHolder(val binding:ListMenuBinding): RecyclerView.ViewHolder(binding.root) {
@@ -69,4 +88,14 @@ class GoodsAdapter(val dataSet: ArrayList<GoodsDTO>): RecyclerView.Adapter<Goods
             }
         }
     }
+
+    class AddViewHolder(val binding: ListMenuAddBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
+            binding.btnPlus.setOnClickListener{
+
+            }
+        }
+    }
+
+
 }
