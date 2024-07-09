849c89f1f389df07e068de7f7cdd90328fd1a14a
Moon Ji-Hee <answlgml6@gmail.com>
2024-07-09 18:52:44 +0900

    2024-07-09

--- app/src/main/java/com/wooriyo/us/pinmenuer/model/OrderHistoryDTO.kt
+++ app/src/main/java/com/wooriyo/us/pinmenuer/model/OrderHistoryDTO.kt
@@ -15,13 +15,17 @@ data class OrderHistoryDTO(
     @SerializedName("isreser") var isreser: Int,                        // 0: 예약 확인 전, 1: 예약 확인 완료
     @SerializedName("glist") var olist : ArrayList<OrderDTO>,           // 주문 상세 리스트
     @SerializedName("rlist") var rlist : ArrayList<ReservationDTO>,     // 예약 정보
-    @SerializedName("amount") var amount : Double,                         // 총 금액
+    @SerializedName("amount") var amount : Double,                      // 상품 총 금액
+    @SerializedName("tip")      var tip : Double,                       // 팁 총 금액
+    @SerializedName("tipPer")   var tipPer : Int,                       // 팁 %
+    @SerializedName("tax")      var tax : Double,                       // 택스 총 금액
+    @SerializedName("taxPer")   var taxPer : Double,                    // 택스 %
     @SerializedName("regdt") var regdt : String,                        // 등록일시 (주문일시)
     @SerializedName("iscompleted") var iscompleted : Int,               // 완료 여부 (1 : 완료, 0 : 미완료)
     @SerializedName("complate_updt") var complate_updt : String,        // 완료일시 (결제일시)
 
     @SerializedName("total_gea") var total : Int,                       // 총 개수
-    @SerializedName("total_price") var total_price : Double,            // 총 금액
+    @SerializedName("total_price") var total_price : Double,            // 상품 + 팁 + 택스 총 금액
 
     @SerializedName("TotalOrdCnt") var totalOrdCnt: Int,                // 테이블별 주문보기 - 주문 개수
     @SerializedName("TotalCallCnt") var totalCallCnt: Int,              // 테이블별 주문보기 - 호출 개수
